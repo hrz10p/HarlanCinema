0df29dcc9c2d162f23de77535567646ca6016242
hrz10p <yerlankuanysh@gmail.com>
2024-03-05 21:16:09 +0500

    refactor service -> repo #2

--- pkg/repos/service.go
+++ pkg/repos/service.go
@@ -5,21 +5,19 @@ import (
 )
 
 type Repository struct {
-	UserRepository    UserRepository
-	SessionRepository SessionRepository
-	MovieRepository   MovieRepository
-	TicketRepository  TicketRepository
-	ReviewRepository  ReviewRepository
-	SeanceRepository  SeanceRepository
+	UserRepository   UserRepository
+	MovieRepository  MovieRepository
+	TicketRepository TicketRepository
+	ReviewRepository ReviewRepository
+	SeanceRepository SeanceRepository
 }
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{
-		UserRepository:    *NewUserRepository(db),
-		SessionRepository: *NewSessionRepository(db),
-		MovieRepository:   *NewMovieRepository(db),
-		TicketRepository:  *NewTicketRepository(db),
-		ReviewRepository:  *NewReviewRepository(db),
-		SeanceRepository:  *NewSeanceRepository(db),
+		UserRepository:   *NewUserRepository(db),
+		MovieRepository:  *NewMovieRepository(db),
+		TicketRepository: *NewTicketRepository(db),
+		ReviewRepository: *NewReviewRepository(db),
+		SeanceRepository: *NewSeanceRepository(db),
 	}
 }
